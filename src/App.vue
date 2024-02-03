30e8c75396c83798a2ea48c1b95c9a62ff89c6b6
Chathura <cpriydasun@gmail.com>
2024-02-04 00:11:36 +0530

    Binding to Attribute

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import HelloWorld from "./components/dynamicTexts/HelloWorld.vue";
+import BindingToAttributes from "./components/Binding to Attributes/BindingToAttributes.vue";
 </script>
 
 <template>
   <div class="firstDiv">{{ greet }} {{ name }}</div>
   <div v-text="channel"></div>
   <HelloWorld />
+  <BindingToAttributes />
 </template>
+
 
 <script>
 export default {
---
+++ src/components/Binding to Attributes/BindingToAttributes.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+<template>
+    <div v-bind:id = "headingId">
+        <h1>Binding to Attributes</h1>
+        <p>Using v-bind to bind the value of id attribute</p>
+        <input type="text" v-bind:disabled="isDisabled">
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "BindingToAttributes",
+  data() {
+    return {
+        headingId : "header",
+        isDisabled : true
+    };
+  },
+};
+</script>
+
+<style scoped>
+    #header {
+        text-align: center;
+        color: blue;
+    }
+</style>
+
+
+
+
