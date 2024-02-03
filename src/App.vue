6adb4186b8bfd58fac8967833a38dda8003876c0
Chathura <cpriydasun@gmail.com>
2024-02-04 02:17:24 +0530

    binding styles

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,23 @@
 import HelloWorld from "./components/dynamicTexts/HelloWorld.vue";
 import BindingToAttributes from "./components/Binding to Attributes/BindingToAttributes.vue";
 import BindingClasses from "./components/BindingClasses/BindingClasses.vue";
+import BindingStyles from "./components/BindingStyles/BindingStyles.vue";
+
 </script>
 
 <template>
   <div class="firstDiv">{{ greet }} {{ name }}</div>
   <div v-text="channel"></div>
+   <hr>
   <HelloWorld />
+   <hr>
   <BindingToAttributes />
+   <hr>
   <BindingClasses />
+  <hr>
+  <BindingStyles />
+  <hr>
+  
 </template>
 
 
---
+++ src/components/BindingStyles/BindingStyles.vue
@@ -0,0 +1,45 @@
+<template>
+  <h2
+    v-bind:style="{
+      color: backColor,
+      fontSize: headerSize + 'px',
+    }"
+  >
+    Style by js Object
+  </h2>
+  <h2 :style="styleObject">Style by js Object</h2>
+
+  <!--apply multiple Styles using array -->
+  <h2 v-bind:style="[styleObject1,styleObject2]"> Styles using array</h2>
+</template>
+
+<script>
+export default {
+  name: "BindingStyles",
+  data() {
+    return {
+      backColor: "burlywood",
+      headerSize: "50",
+
+      styleObject: {
+        color: "red",
+        fontSize: "50px",
+        fontFamily: "Arial",
+      },
+
+      styleObject1: {
+        fontSize: "50px",
+        fontFamily: "Arial",
+      },
+
+      styleObject2: {
+        color: "blue",
+        border: "2px solid black",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
