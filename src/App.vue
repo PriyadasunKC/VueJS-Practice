78c01ad307f65001a042341b8e4fdd0b4588d910
Chathura <cpriydasun@gmail.com>
2024-02-03 23:52:17 +0530

    First

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from "./components/dynamicTexts/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="firstDiv">{{ greet }} {{ name }}</div>
+  <div v-text="channel"></div>
+  <HelloWorld />
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Chathura",
+      greet: "Hello",
+      channel: "CodEvo",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.firstDiv {
+  color: red;
+  text-align: center;
+}
+</style>
---
+++ src/components/dynamicTexts/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import dyStyles from "./dynmicStyles.module.css";
+</script>
+
+<template>
+    <div v-html="msg" :class="dyStyles.hw"></div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "<b>Hello World</b>",
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
